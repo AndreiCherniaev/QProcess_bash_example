21b38657e0cd60bbca183559c5f2a19b24b44293
AndreiCherniaev <glscene2010@gmail.com>
2024-04-21 01:45:20 +0900

    add example

--- src/MainClass.cpp
+++ src/MainClass.cpp
@@ -3,4 +3,38 @@
 MainClass::MainClass(QObject *parent)
     : QObject(parent)
 {
+    //Thanks https://github.com/AndreiCherniaev/libav_MJPEG-transcode-VP9_C_Universe
+    process = new QProcess(this);
+    connect(process, &QProcess::errorOccurred, this, &MainClass::QProcessErrHandler);
+    connect(process, &QProcess::stateChanged, this, &MainClass::QProcessStateChangeHandler);
+    connect(process, &QProcess::finished, this, &MainClass::QProcessFinishHandler);
+    process->setProcessChannelMode(QProcess::MergedChannels);
+    //If you need read Linux env variables (for example $PWD) then use qgetenv("PWD");
+    process->setProgram("bash");
+    process->setArguments({"-c", "ffmpeg -y -f lavfi -i testsrc=size=1280x720:rate=1:duration=10 -vcodec mjpeg -pix_fmt yuvj422p -f mjpeg input.yuvj422p"});
+    process->start();
+}
+
+
+void MainClass::QProcessErrHandler(QProcess::ProcessError error){
+    qWarning().noquote().nospace() << "QProcess problem;" << error;
+    QCoreApplication::quit();
+}
+
+
+void MainClass::QProcessStateChangeHandler(QProcess::ProcessState newState){
+    qDebug() << "new QProcess state=" << newState;
+    if(newState==QProcess::NotRunning){
+        qDebug().noquote().nospace() << "QProcess should finish";
+    }
+}
+
+
+void MainClass::QProcessFinishHandler(int exitCode, QProcess::ExitStatus exitStatus){
+    QByteArray processMsg(process->readAll());
+    if(exitCode== 0 && exitStatus== QProcess::NormalExit){
+        if(processMsg.isEmpty()) qDebug().noquote().nospace() << "QProcess finish ok";
+        else qDebug().noquote().nospace() << "QProcess finish ok;msg;" << processMsg;
+    }else qWarning().noquote().nospace() << "QProcess finish with code;" << exitCode << ";exitStatus;" << exitStatus << ";msg;" << processMsg;
+    QCoreApplication::quit();
 }
--- src/MainClass.h
+++ src/MainClass.h
@@ -2,13 +2,17 @@
 
 #include <QCoreApplication>
 #include <QObject>
-#include <QTimer>
 #include <QDebug>
+#include <QProcess>
 
 class MainClass : public QObject
 {
     Q_OBJECT
-
+    QProcess *process;
 public:
     MainClass(QObject *parent);
+private slots:
+    void QProcessErrHandler(QProcess::ProcessError error);
+    void QProcessStateChangeHandler(QProcess::ProcessState newState);
+    void QProcessFinishHandler(int exitCode, QProcess::ExitStatus exitStatus);
 };
